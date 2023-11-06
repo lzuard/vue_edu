7b71ce94f8c671e1f613c70b870757f363b3098e
Izuard <sasha.20133@gmail.com>
2023-11-06 12:55:46 +0300

    add postForm  and postList

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,48 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="app">
+    <post-form @create="createPost"/>
+    <post-list :posts="posts"/>
+  </div>
 </template>
 
+<script>
+import PostForm from "@/components/PostForm.vue"
+import PostList from "@/components/PostList.vue"
+
+export default{
+  components: {
+    PostForm, PostList
+  },
+  data() {
+    return {
+      posts: [
+      {id: 1, title:'Title 1', body:'Description 1'},
+      {id: 2, title:'Title 2', body:'Description 2'},
+      {id: 3, title:'Title 3', body:'Description 3'},
+      {id: 4, title:'Title 4', body:'Description 4'},
+    ],
+    title:'',
+    body: '',
+    } 
+    
+  },
+  methods:{
+    createPost(post){
+      this.posts.push(post)
+    }
+  }
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+body{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
 
-nav {
-  padding: 30px;
+.app{
+  margin: 15px;
 }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
