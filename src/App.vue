ac5c39097166d5e82b2cc5ffc9e5d9402541ac28
Izuard <sasha.20133@gmail.com>
2023-11-06 14:53:55 +0300

    add dialog element

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="app">
-    <post-form @create="createPost"/>
+    <h2>Страница с постами</h2>
+    <my-button @click="showDialog">Создать пост</my-button>
+    <my-dialog v-model:show="dialogVisible">
+      <post-form @create="createPost"/>
+    </my-dialog>
     <post-list 
       :posts="posts"
       @remove="removePost"  
@@ -24,17 +28,20 @@ export default{
       {id: 3, title:'Title 3', body:'Description 3'},
       {id: 4, title:'Title 4', body:'Description 4'},
     ],
-    title:'',
-    body: '',
+    dialogVisible: false
     } 
     
   },
   methods:{
     createPost(post){
       this.posts.push(post)
+      this.dialogVisible=false
     },
     removePost(post){
       this.posts = this.posts.filter(p=> p.id !== post.id)
+    },
+    showDialog(){
+      this.dialogVisible=true
     }
   }
 }
